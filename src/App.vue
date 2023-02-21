5df8f59a34a0ca614ab2ce43e23a465c20490b2c
erenkayacan <erenkayacan@outlook.com.tr>
2023-02-21 11:16:02 +0300

    feat: routes added, default route changed and views component names changed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div>
+    <nav>
+      <router-link to="/list" class="nav-link">Product List</router-link>
+      <router-link to="/cart" class="nav-link"
+        >Cart({{ basketItemCount || 0 }})</router-link
+      >
+    </nav>
+    <router-view />
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  computed: {
+    basketItemCount() {
+      return this.$store.state.cart.length;
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -16,14 +31,36 @@
 }
 
 nav {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
   padding: 30px;
 
-  a {
+  .nav-link {
     font-weight: bold;
-    color: #2c3e50;
+    color: black;
+    text-decoration: none;
+    margin: 10px;
+
+    &:hover {
+      color: orange;
+      text-decoration: underline;
+    }
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: orange;
+      text-decoration: underline;
+    }
+  }
+}
+
+@media screen and (max-width: 768px) {
+  nav {
+    justify-content: space-between;
+
+    .nav-link {
+      margin: 0;
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import CartListView from '../views/CartView.vue';
+import ProductListView from '../views/ProductListView.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView,
+    redirect: '/list',
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: '/list',
+    name: 'list',
+    component: ProductListView,
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: CartListView,
   },
 ];
 
