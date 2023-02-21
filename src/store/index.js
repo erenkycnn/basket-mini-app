477c7f58756b2359c87d4bbc139b80682dfcd49b
erenkayacan <erenkayacan@outlook.com.tr>
2023-02-21 11:09:00 +0300

    feat: vuex store created for crud

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,58 @@
 import { createStore } from 'vuex';
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+const store = createStore({
+  state: {
+    products: [],
+    cart: [],
+    cartValue: 0,
+  },
+  mutations: {
+    setProducts(state, products) {
+      state.products = products;
+    },
+    addToCart(state, { product, quantity }) {
+      state.cartValue += product.price * quantity;
+      const existingProduct = state.cart.find((p) => p.id === product.id);
+      if (existingProduct) {
+        state.cart[state.cart.indexOf(existingProduct)].quantity += quantity;
+        return;
+      }
+      state.cart.push({ ...product, quantity });
+    },
+    removeCartItem(state, productId) {
+      const existingProduct = state.cart.find((p) => p.id === productId);
+      if (existingProduct) {
+        state.cartValue -= existingProduct.price * existingProduct.quantity;
+        state.cart = state.cart.filter((p) => p.id !== productId);
+      }
+    },
+    updateCartItemQuantity(state, { productId, quantity }) {
+      const existingProduct = state.cart.find((p) => p.id === productId);
+      if (existingProduct) {
+        state.cartValue -= existingProduct.price * existingProduct.quantity;
+        existingProduct.quantity = quantity;
+        state.cartValue += existingProduct.price * existingProduct.quantity;
+      }
+    },
+  },
+  actions: {
+    async fetchProducts({ commit }, products) {
+      commit('setProducts', products);
+    },
+    async addToCart({ commit }, { product, quantity }) {
+      commit('addToCart', { product, quantity });
+    },
+    removeCartItem({ commit }, product) {
+      commit('removeCartItem', product.id);
+    },
+    updateCartItemQuantity({ commit }, { productId, quantity }) {
+      commit('updateCartItemQuantity', { productId, quantity });
+    },
+  },
+  getters: {
+    cartProducts: (state) => state.cart,
+    cartTotalPrice: (state) => state.cartValue,
+  },
 });
+
+export default store;
