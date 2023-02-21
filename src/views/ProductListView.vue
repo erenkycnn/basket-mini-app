8a61299711dcabb74369d9717cd7da995edda553
erenkayacan <erenkayacan@outlook.com.tr>
2023-02-21 11:59:06 +0300

    feat: toast added for add to cart request

--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <ProductList :products="products" @add-to-cart="handleAddToCart" />
+    <Toast v-if="showToast" :message="toastMessage" :type="toastType" />
   </div>
 </template>
 
@@ -8,10 +9,19 @@
 import { mapState, mapActions } from 'vuex';
 import ProductService from '@/services/productService';
 import ProductList from '@/components/Products/ProductList';
+import Toast from '@/components/Toast';
 
 export default {
   components: {
+    Toast,
     ProductList,
+  },
+  data() {
+    return {
+      showToast: false,
+      toastMessage: '',
+      toastType: '',
+    };
   },
   computed: mapState(['products']),
   async created() {
@@ -28,8 +38,19 @@ export default {
       try {
         await ProductService.addToCart(product.id, quantity);
         this.addToCart({ product, quantity });
+        this.showToast = true;
+        this.toastMessage = `${quantity} ${product.name} added to cart`;
+        this.toastType = 'success';
+        setTimeout(() => {
+          this.showToast = false;
+        }, 3000);
       } catch (error) {
-        console.log('error', error);
+        this.showToast = true;
+        this.toastMessage = `${quantity} ${product.name} is out of stock`;
+        this.toastType = 'error';
+        setTimeout(() => {
+          this.showToast = false;
+        }, 3000);
       }
     },
   },
